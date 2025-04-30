af68248c41c1a938309e6d8dd9e5f1345ceac03c
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-04-30 16:12:21 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout/>}>
-        <Route path="/Service" element={<Service/>}/>
+        <Route path="/" element={<Service/>}/>
         </Route>
       </Routes>
     </Router>
