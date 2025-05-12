0db77f3c0969dbc0bb98f9178321f0cf815af86b
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-05-12 13:23:04 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Service from './pages/service'
 import Layout from './layout';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Contact from './pages/contact';
+// import Contact from './pages/contact';
 function App() {
 
   return (
@@ -10,8 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
         <Route path="/" element={<Service/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/services" element={<Service/>}/>
+        {/* <Route path="/contact" element={<Contact/>}/>
+        <Route path="/services" element={<Service/>}/> */}
         </Route>
       </Routes>
     </Router>
